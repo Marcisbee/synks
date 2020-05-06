285165a83572d61c2fa74d955926372891ab345a
Marcisbee <marcisbergmanis@gmail.com>
2020-05-06 21:40:35 +0300

    Handle `innerHTML` and `style` attributes

--- src/patch-props.ts
+++ src/patch-props.ts
@@ -1,12 +1,53 @@
 import { VNode } from '../types';
 
+enum PROPS {
+  innerHTML = 'innerHTML',
+  style = 'style',
+}
+
+function patchStyle(currentNode: VNode, previousNode: VNode): void {
+  const target = currentNode.target;
+  if (!currentNode.type || (target instanceof Text) || !target) return;
+
+  const styleA = (currentNode.props || {}).style || {};
+  const styleB = (previousNode.props || {}).style || {};
+
+  Object.keys(styleA).forEach(name => {
+    const value = styleA[name];
+    const oldValue = styleB[name];
+    if (value === oldValue) return;
+    if (value === null || value === false || value === undefined) {
+      return delete target[name];
+    }
+
+    return target.style[name] = String(value);
+  });
+
+  if (!previousNode) return;
+
+  Object.keys(styleB).forEach(name => {
+    if (styleA && (
+      styleA[name] !== null
+      && styleA[name] !== false
+      && styleA[name] !== undefined
+    )) return;
+
+    return delete target.style[name];
+  });
+}
+
 export function patchProps(currentNode: VNode, previousNode: VNode): void {
   const target = currentNode.target;
   if (!currentNode.type || (target instanceof Text) || !target) return;
 
-  Object.keys(currentNode.props || {}).forEach(name => {
-    const value = currentNode.props[name];
-    const oldValue = previousNode.props && previousNode.props[name];
+  const propsA = currentNode.props || {};
+  const propsB = previousNode.props || {};
+
+  Object.keys(propsA).forEach(name => {
+    if (name === PROPS.style) return;
+
+    const value = propsA[name];
+    const oldValue = propsB[name];
     if (value === oldValue) return;
     if (value instanceof Function) {
       if (value === target[name]) return;
@@ -15,19 +56,34 @@ export function patchProps(currentNode: VNode, previousNode: VNode): void {
       if (value === null || value === false || value === undefined) {
         return target.removeAttribute(name);
       }
-      return target.setAttribute(name, String(value));
+
+      const newValue = String(value);
+
+      if (name === PROPS.innerHTML) {
+        return target[name] = newValue;
+      }
+
+      return target.setAttribute(name, newValue);
     }
   });
 
   if (!previousNode) return;
 
-  Object.keys(previousNode.props || {}).forEach(name => {
-    if (currentNode.props && (
-      currentNode.props[name] !== null
-      && currentNode.props[name] !== false
-      && currentNode.props[name] !== undefined
+  Object.keys(propsB).forEach(name => {
+    if (name === PROPS.style) return;
+
+    if (propsA && (
+      propsA[name] !== null
+      && propsA[name] !== false
+      && propsA[name] !== undefined
     )) return;
+
+    if (name === PROPS.innerHTML) {
+      return delete target[name];
+    }
 
     return target.removeAttribute(name);
   });
+
+  patchStyle(currentNode, previousNode);
 }
